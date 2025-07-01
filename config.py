f8b4751cbfc20df785ab1584c6f399d36ff54945
Mansour A <44963644+mansour2002@users.noreply.github.com>
2025-07-01 09:58:46 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 import os
 
 # Dataset and Project Configuration
-PARENT_DIR = 'D:\\AV Project\\CFZ-Net'  # Please update to your project's root directory
+PARENT_DIR = ''  # Please update to your project's root directory
 SAVE_FOLDER_NAME = "CFZ_Segementation_TransUnet_V01T01"
-DATAFRAME_NAME = 'train_data_tmp_Idea61CFZ.csv'
+DATAFRAME_NAME = 'train_data_CFZ.csv'
 IMAGESIZE = 320  # Set the standard image size for resizing images
 
 # Derived Paths (do not modify directly)
 SAVE_PATH = os.path.join(PARENT_DIR, SAVE_FOLDER_NAME)
-PROJECT_PATH = os.path.join(PARENT_DIR, 'tmp_Idea61CFZ')
+PROJECT_PATH = os.path.join(PARENT_DIR, 'tmp_CFZ')
 DATASET_PATH = os.path.join(PROJECT_PATH, 'Dataset')
 TRAIN_PATH = os.path.join(DATASET_PATH, 'train')
 INPUT_PATH = os.path.join(TRAIN_PATH, 'Input')
@@ -30,4 +30,4 @@ FOLD_NUM = 5  # Current fold for cross-validation
 MAX_FOLD = 5  # Total number of folds
 
 # Data Source (if using the data transfer function)
-SOURCE_DIR = "D:\\AV Project\\Dataset6x6mm\\" # Please update to your raw data source directory
+SOURCE_DIR = "" # Please update to your raw data source directory
